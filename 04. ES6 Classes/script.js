84c953c8bbb6e7e732c7d4a2bbd29fecb8ea4159
ephraim <ephraims.818@gmail.com>
2025-06-02 14:57:22 +0600

    JS-REV--OOP - 04. ES6 CLasses

---
+++ 04. ES6 Classes/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+jessica.greet();
