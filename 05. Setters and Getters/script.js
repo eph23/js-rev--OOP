26cc0c602471dd8cc3425cae916334935fa72c8a
ephraim <ephraims.818@gmail.com>
2025-06-02 15:24:01 +0600

    JS-REV--OOP - 05. Setters and Getters

---
+++ 05. Setters and Getters/script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) {
+      this._fullName = name;
+    } else {
+      alert(`${name} is not a full name`);
+    }
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+console.log(jessica._fullName);
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+jessica.greet();
+
+const walter = new PersonCl('Walter S', 1965);
+console.log(walter);
+
+const account = {
+  name: 'Eph',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account);
+console.log(account.latest);
+account.latest = 50;
+
+console.log(account.latest);
+console.log(account.movements);
